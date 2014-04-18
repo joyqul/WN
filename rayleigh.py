487dc046398ef550a04709b143506ac696c5cdf6
Joyqul Chen <joy1357924680@gmail.com>
2014-04-18 09:52:16 +0800

    Maybe finish part 1? According to [here](http://www.edaboard.com/thread71584.html)

--- rayleigh.py
+++ rayleigh.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import random
 
 def stop_and_wait(pb, d, rb, n, k):
     return ((1 - pb)** n / (1 + d*rb / n)) * (k / n)
@@ -14,59 +15,123 @@ def go_back_n(pb, n, k, N):
 def selective_repeat(pb, n, k):
     return (1 - pb) ** n * (k / n)
 
-def probability_density_function(r, theta):
-    theta = theta ** 2
-    if theta == 0:
-        return 0
-    return (r / theta) * (math.e ** (- r**2 / (2 * theta)))
 
-def urban_path_loss(dis, alpha = 0.8, fc = 1000, hb = 30, hm = 1):
-    # fc: carrier frequency (150 MHz to 1500 MHz) 
-    # hb: effective BS antenna height(30m ~ 200m)
-    # hm: effective MS antenna height(1m ~ 10m)
-    # dis: distance (1m ~ 20km)
-    # alpha: correction factor for the mobile antenna height
-    return 69.55 + 26.16 * math.log(fc, 10) - 13.82 * math.log(hb, 10) - \
-        alpha + (44.9 - 6.55 * math.log(hb, 10)) * math.log(dis, 10)
-
-def large_cities_loss(dis, fc = 1000, hb = 30, hm = 1):
-    alpha = (1.1 * math.log(fc, 10) - 0.7) * hm - (1.56 * math.log(fc, 10) - 0.8)
-    return urban_path_loss(dis, alpha)
-
-def medium_small_loss(dis, fc = 1000, hb = 30, hm = 1):
-    if fc > 300:
-        alpha = 8.29 * (math.log(1.54 * hm, 10))**2 - 1.1
-    else:
-        alpha = 3.2 * (math.log(11.75 * hm, 10))**2 - 4.97
-    return urban_path_loss(dis, alpha)
-def suburban_loss(dis, alpha = 0.8, fc = 1000, hb = 30, hm = 1):
-    return urban_path_loss(dis) - 2 * (math.log(fc / 28, 10))**2 - 5.4
-
-def open_loss(dis, alpha = 0.8, fc = 1000, hb = 30, hm = 1):
-    return urban_path_loss(dis) - 4.78 * math.log(fc, 10)**2 - 18.33 * math.log(fc, 10) - 40.94
+def randn():
+    return random.gauss(0, 1)
     
 if __name__ == '__main__':
-    """Set the control data"""
-    # theta = E(r**2)
-    theta = 1
-    # Gt, transmitting antenna gain
-    Gt = 1
-    # Gr, receiving antenna gain
-    Gr = 1
-    # Pt, transmitting signal power
-    # Pr, receiving signal power
-    # Lp, path loss 
-    # Ls, slow fading
-    # Lf, fast fading
-    # L, propagation loss
-    # L = Lp * Ls * Lf
-    # Pr = (Gt * Gr * Pt) / L
+    N = 256 # Number of frequency samples
+    M = 8192 # Number of time samples
 
-    """Plot"""
-    x = np.linspace(0.001, 10, 100000)
-    # y = [probability_density_function(i, theta) for i in x]
-    y = [open_loss(i) for i in x]
-    plt.axis([0, 10, 0, 200])
-    plt.plot(x, y)
-    plt.show()
+    # Required parameters for INPUT: fm and row (r0)
+    fm = int(raw_input("ENTER THE VALUE OF fm [20 Hz, 200Hz]:"))
+    r0 = float(raw_input("ENTER THE VALUE OF r0 [1,0.1,0.01]:"))
+
+    y = 1
+    Afd_p = 0 # Average fade duration; practical value
+    Nr_p = 0 # Number of Zero-crossing level per second
+    Rrms_p=0 # Practically calculated R-rms value
+    
+    while y <= 1:
+        delta_f = 2 * fm / float(N) # Frequency resolution
+        delta_t = float(N) / (M-1) /2 /fm # Time resolution
+
+        """ If N = M-1, then the time resolution delta_t = 1 / 2 * fm, which may not be small so take M >> N. 
+    When M > N, we need to pad with zero values before taking IFFT.  """
+
+        X = [complex(randn(), randn()) for m in xrange(N/2 + 1)]
+        Y = [complex(randn(), randn()) for m in xrange(N/2 + 1)]
+
+        X[0] = 0
+        Y[0] = 0
+
+        for i in xrange(N/2 + 1, M - N/2 - 1):
+            X.append(0)
+            Y.append(0)
+
+        for m in xrange(M - N/2 - 1, M):
+            X.append(X[M - m].conjugate())
+            Y.append(Y[M - m].conjugate())
+
+
+        # Sample Se(f) Spectrum
+        SeF = [1.5 / (math.pi * fm * (math.sqrt(1 - ( (i - 1) * float(delta_f) / fm )**2))) for i in xrange(N/2)]
+
+        # Calculating Edge Value by extending the slope prior to passband edge to edge
+        SeF.append(SeF[N/2 -1] * 2 - SeF[(N/2 - 1) - 1])
+        
+        for i in xrange(N/2 + 1, M - N/2):
+            SeF.append(0)
+
+        for m in xrange(M - N/2, M):
+            SeF.append(SeF[M - m])
+    
+        X_shaped = [ X[m] * math.sqrt(SeF[m]) for m in xrange(M) ]
+        Y_shaped = [ Y[m] * math.sqrt(SeF[m]) for m in xrange(M) ]
+
+        X_shaped = np.fft.ifft(X_shaped)
+        Y_shaped = np.fft.ifft(Y_shaped)
+        X_component = [ m.real for m in X_shaped ]
+        Y_component = [ m.real for m in Y_shaped ]
+
+        #************* Find R-rms value and envelope of Rayleigh Distribution ***********%
+
+        R = [math.sqrt(X_component[m]** 2 + Y_component[m]**2) for m in xrange(M)]
+        r = [20 * math.log(m, 10) if m!=0 else 0 for m in R]
+
+        rms = math.sqrt(sum(m**2 for m in R)/len(R))
+        Rrms = 20 * math.log(rms, 10)
+        level = 20 * math.log(r0*rms, 10)
+
+        R = [ m - Rrms for m in r]
+
+        plt.axis([1, 8192, -60, 20])
+        plt.plot(R)
+        plt.show()
+
+        # Calculating (Practically) Number of Zero Level Crossing and Average Fade Duration
+
+        h = 0
+        c = 0
+        C1 = 0
+        NUM = 0
+
+        while h <= M-1:
+            if r[h] <= level:
+                i = h
+                while i <= M-1:
+                    if r[i] >= level:
+                        NUM = NUM + 1
+                        break
+                    i = i + 1
+
+                c = i - h
+                C1 = C1 + c
+                h = i - 1
+
+            h = h + 1
+        
+        Afd_p = Afd_p + (C1/NUM) * delta_t
+        Nr_p = Nr_p + NUM * delta_f
+        Rrms_p = Rrms_p + Rrms
+
+        y = y + 1
+
+    # ************ Theoretical calculation of Number of Zero Level Crossing (Nr) and Average Fade Duration ************* %
+
+    Nr_theoretical = math.sqrt(2 * math.pi) * fm * r0 *math.exp(-r0**2)
+    
+    z1 = math.exp(r0**2) - 1
+    z2 = r0 * fm * math.sqrt(2 * math.pi)
+    Average_fade_duration_theoretical = z1 / z2
+
+    rowdb = 10 * math.log(r0, 10)
+    Rrms_theoretical = Rrms + rowdb
+    print Average_fade_duration_theoretical, Afd_p
+    
+    values = plt.plot(np.random.rayleigh(3, 1000))
+    meanvalue = 1
+    modevalue = np.sqrt(2 / np.pi) * meanvalue
+    s = np.random.rayleigh(modevalue, 1000000)
+    
     
